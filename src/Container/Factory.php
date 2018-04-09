436bd0747a8066d5fca8600706c16558809978d6
Rasmus Tamm <rasmus.tamm@ekspressdigital.eu>
2018-04-09 19:12:12 +0300

    Add Factory

---
+++ src/Container/Factory.php
@@ -0,0 +1,33 @@
+<?php
+namespace MagicDI\Container;
+
+use InvalidArgumentException;
+
+class Factory extends Container
+{
+    /**
+     * @param $name
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function __get($name)
+    {
+        $hasProperty = $this->hasProperty($name);
+
+        if ($hasProperty === null) {
+            throw new InvalidArgumentException("Property {$name} not found!");
+        }
+
+        if ($hasProperty !== $this) {
+            return $hasProperty->$name;
+        }
+
+        $output = $this->$name;
+
+        if (!is_callable($output)) {
+            return $output;
+        }
+
+        return call_user_func($output($this->parentContainer));
+    }
+}
